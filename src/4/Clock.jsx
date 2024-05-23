882c7d908e0fb57843cc53fda1e8447b7cce7c8d
joymet33 <joymet33@gmail.com>
2024-05-23 11:24:23 +0900

    feat: Clock.jsx 완료

--- src/4/Clock.jsx
+++ src/4/Clock.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import { useState } from "react";
 
 const Clock = () => {
+  const [show, setShow] = useState(false);
+
+  setInterval(() => {
+    // setShow(!show)
+    setShow((prev) => !prev);
+  }, 1000);
+
   return (
     <div>
       <h1>안녕, 리액트!</h1>
-      <h2>현재 시간: {new Date().toLocaleTimeString()}</h2>
+      <h2>현재 시간: {show && new Date().toLocaleTimeString()}</h2>
     </div>
   );
 };
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
 import "./App.css";
 import Clock from "./4/Clock.jsx";
-import { useState } from "react";
 
 function App() {
-  const [show, setShow] = useState(false);
-
-  setInterval(() => {
-    // setShow(!show)
-    setShow((prev) => !prev);
-  }, 1000);
-
-  return show && <Clock />;
+  return <Clock />;
 }
 
 export default App;
